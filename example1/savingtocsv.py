3a94b8f00e673924ff4a42558fea83b5a789d5ef
Marouane <marouane.chouhou@tttech-auto.com>
2022-09-04 22:35:27 +0200

    ABC-123 draft

---
+++ example1/savingtocsv.py
@@ -0,0 +1,20 @@
+import csv
+import random
+import time
+import os
+
+os.remove('demo_csv1.csv')
+column_name = ["time", "cpu load"]
+xaxis = 0
+with open('demo_csv1.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(column_name)
+    f.close()
+while True:  
+    with open('demo_csv1.csv', 'a') as f:
+        writer = csv.writer(f)  
+
+        writer.writerow([xaxis, random.randint(1,10)])
+        xaxis += 1
+        
+        time.sleep(1)
